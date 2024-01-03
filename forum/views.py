29e966dba289323520bb1e49bdea4604225f6694
Saber <dj.saber.mharesh@gmail.com>
2024-01-03 16:26:13 +0300

    add listview

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Question, Answer
 
-# Create your views here.
+
+def question_list(request):
+    data = Question.objects.all()
+    return render(request, 'forum\list.html', {'data':data})
+
+
--- project/urls.py
+++ project/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from forum.views import question_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('questions/' , question_list),
 ]
 
 urlpatterns += static(settings.STATIC_URL ,document_root=settings.STATIC_ROOT)
