1b71419b0c95fca2ffd3bab99bbbf0073a846df4
Saber <dj.saber.mharesh@gmail.com>
2024-01-03 17:28:12 +0300

    add answer form

--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Question, Answer
+from .froms import AnswerForm
 
 
 def question_list(request):
@@ -9,9 +10,21 @@ def question_list(request):
 
 def question_detail(request, id):
     question = Question.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            myform = form.save(commit=False)
+            myform.author = request.user
+            myform.question = question
+            myform.save()
+
+            #form = AnswerForm()
+    else:
+        form = AnswerForm()
     
 
-    return render(request, 'forum\detail.html', {'question':question})
+    return render(request, 'forum\detail.html', {'question':question, 'form':form})
 
 
 
